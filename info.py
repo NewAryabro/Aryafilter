0df7541dd5b1f2bf5c6eae72cd4cc52a1a407284
NewAryabro <138558338+NewAryabro@users.noreply.github.com>
2023-08-14 09:43:20 +0530

    Update info.py

--- info.py
+++ info.py
@@ -21,7 +21,7 @@ BOT_TOKEN = environ.get('BOT_TOKEN', "")
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', False))
-PICS = (environ.get('PICS', 'https://telegra.ph/file/7e56d907542396289fee4.jpg https://telegra.ph/file/9aa8dd372f4739fe02d85.jpg https://telegra.ph/file/adffc5ce502f5578e2806.jpg https://telegra.ph/file/6937b60bc2617597b92fd.jpg https://telegra.ph/file/09a7abaab340143f9c7e7.jpg https://telegra.ph/file/5a82c4a59bd04d415af1c.jpg https://telegra.ph/file/323986d3bd9c4c1b3cb26.jpg https://telegra.ph/file/b8a82dcb89fb296f92ca0.jpg https://telegra.ph/file/31adab039a85ed88e22b0.jpg https://telegra.ph/file/c0e0f4c3ed53ac8438f34.jpg https://telegra.ph/file/eede835fb3c37e07c9cee.jpg https://telegra.ph/file/e17d2d068f71a9867d554.jpg https://telegra.ph/file/8fb1ae7d995e8735a7c25.jpg https://telegra.ph/file/8fed19586b4aa019ec215.jpg https://telegra.ph/file/8e6c923abd6139083e1de.jpg https://telegra.ph/file/0049d801d29e83d68b001.jpg')).split()
+PICS = (environ.get('PICS', 'https://telegra.ph/file/1d17409b2d3b3f8e5b832.jpg https://telegra.ph/file/e789fa3592124f5f171cd.jpg https://telegra.ph/file/6aebfd481564de3b78a7b.jpg https://telegra.ph/file/7342f3f35baed0aaa0c1f.jpg https://telegra.ph/file/f219bfd16349a9eafe8ab.jpg https://telegra.ph/file/283e29d87ae7c6366e6ef.jpg https://telegra.ph/file/32822474de444dc1238c8.jpg https://telegra.ph/file/b0e11cd2b1dec312e121a.jpg https://telegra.ph/file/1ba91e67442a38d563c12.jpg https://telegra.ph/file/3b6e7dca86a1254bf2d53.jpg')).split()
 
 # Admins, Channels & Users
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '').split()]
